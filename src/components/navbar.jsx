2293e4f740fab103b60e84eb98ccdc81473649c9
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2024-11-26 13:00:14 +0500

    updates

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,84 +1,104 @@
-import { useEffect, useState } from 'react';
-import Logo from '../assets/logo.svg';
+import { useEffect, useState } from "react";
 import { MdOutlineMenu } from "react-icons/md";
-import { Link } from 'react-scroll'
-
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [showMenu, setShowMenu] = useState(false)
+    const [showMenu, setShowMenu] = useState(false);
 
     useEffect(() => {
-        document.body.style.overflow = showMenu ? 'hidden' : ''
+        document.body.style.overflow = showMenu ? "hidden" : "";
     }, [showMenu]);
 
     return (
-
         <>
-            <div className='bg-white md:bg-opacity-40 md:backdrop-blur-3xl h-full  md:shadow-lg z-50 md:border-b md:border-white/20 w-full sticky top-0'>
+            <div className="bg-white md:bg-opacity-40 md:backdrop-blur-3xl h-full  md:shadow-lg z-50 md:border-b md:border-white/20 w-full sticky top-0">
                 <nav className="max-w-7xl mx-auto bg-white md:bg-transparent p-4 sticky top-0 flex justify-between items-center">
                     <Link
-                        to='/'
-                        activeClass='active'
+                        to="/"
+                        activeClass="active"
                         span={true}
                         smooth={true}
-                        className='cursor-pointer'
+                        className="cursor-pointer"
                     >
-                        <span className='flex items-center font-primary gap-2'>
-                            <img src={Logo} alt="" />
-                            <h3 className='font-bold text-black md:text-xl lg:text-2xl'>Jiandikie</h3>
+                        <span className="flex items-center font-primary gap-2">
+                            <img src={"/assets/logo.svg"} alt="" />
+                            <h3 className="font-bold text-black md:text-xl lg:text-2xl">
+                                Jiandikie
+                            </h3>
                         </span>
                     </Link>
-                    <div className='hidden md:flex gap-6 items-center'>
-                        {
-                            ['about us', 'What you can do', 'Price', 'Tutorial', 'Blog']?.map((i, idx) => (
-                                <Link
-                                    key={i + idx}
-                                    to={i?.split(' ').join('-').toLowerCase()}
-                                    activeClass='active'
-                                    span={true}
-                                    smooth={true}
-                                    className='capitalize cursor-pointer font-bold mr-4 text-lg lg:text-xl'>
-                                    {i}
-                                </Link>
-                            ))
-                        }
-
+                    <div className="hidden md:flex gap-6 items-center">
+                        {[
+                            "about us",
+                            "What you can do",
+                            "Price",
+                            "Tutorial",
+                            "Blog",
+                        ]?.map((i, idx) => (
+                            <Link
+                                key={i + idx}
+                                to={i?.split(" ").join("-").toLowerCase()}
+                                activeClass="active"
+                                span={true}
+                                smooth={true}
+                                className="capitalize cursor-pointer font-bold mr-4 text-lg lg:text-xl"
+                            >
+                                {i}
+                            </Link>
+                        ))}
 
                         <button className="inline-block text-[#116655] font-semibold py-2 px-4 rounded-full bg-white shadow-lg transition duration-300 md:text-lg md:px-6">
                             Get Started
                         </button>
-
                     </div>
-                    <span onClick={() => setShowMenu(!showMenu)} className={`${showMenu ? "rotate-90" : ''} md:hidden  duration-300`}>
+                    <span
+                        onClick={() => setShowMenu(!showMenu)}
+                        className={`${
+                            showMenu ? "rotate-90" : ""
+                        } md:hidden  duration-300`}
+                    >
                         <MdOutlineMenu size={30} />
                     </span>
                 </nav>
 
-
-                <div className={`md:hidden md:relative w-full p-8 min-h-screen right-0 absolute transition-all delay-500  ease-in-out duration-700 bg-white ${showMenu ? "translate-x-0" : '-translate-x-full'}`}>
-                    <div className='self-end flex justify-end text-right flex-col items-end'>
+                <div
+                    className={`md:hidden md:relative w-full p-8 min-h-screen right-0 absolute transition-all delay-500  ease-in-out duration-700 bg-white ${
+                        showMenu ? "translate-x-0" : "-translate-x-full"
+                    }`}
+                >
+                    <div className="self-end flex justify-end text-right flex-col items-end">
                         <ul>
-                            {
-                                ['about us', 'What you can do', 'Price', 'Tutorial', 'Blog']?.map((i, idx) => (
-                                    <Link
-                                        key={i + idx}
-                                        onClick={() => setShowMenu(false)}
-                                        to={i?.split(' ').join('-').toLowerCase()}
-                                        activeClass='active'
-                                        span={true}
-                                        smooth={true}
-                                        className='font-bold cursor-pointer block capitalize mb-3 text-xl' >{i}</Link>
-                                ))
-                            }
+                            {[
+                                "about us",
+                                "What you can do",
+                                "Price",
+                                "Tutorial",
+                                "Blog",
+                            ]?.map((i, idx) => (
+                                <Link
+                                    key={i + idx}
+                                    onClick={() => setShowMenu(false)}
+                                    to={i?.split(" ").join("-").toLowerCase()}
+                                    activeClass="active"
+                                    span={true}
+                                    smooth={true}
+                                    className="font-bold cursor-pointer block capitalize mb-3 text-xl"
+                                >
+                                    {i}
+                                </Link>
+                            ))}
                         </ul>
-                        <button href="#" className="inline-block bg-gradient-to-r from-[#5ac1ac] to-[#116655] text-white font-semibold py-2 px-4 rounded-full hover:bg-teal-600 transition duration-300 md:text-lg md:px-6">
+                        <button
+                            href="#"
+                            className="inline-block bg-gradient-to-r from-[#5ac1ac] to-[#116655] text-white font-semibold py-2 px-4 rounded-full hover:bg-teal-600 transition duration-300 md:text-lg md:px-6"
+                        >
                             Get Started
                         </button>
                     </div>
                 </div>
-            </div >
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/what-you-can-do.jsx
+++ src/components/what-you-can-do.jsx
@@ -1,46 +1,95 @@
-import Intro from '../assets/intro.svg'
+import {
+    Lightbulb,
+    List,
+    Snowflake,
+    FileText,
+    BookOpen,
+    FilePlus,
+    Mail,
+    Megaphone,
+    User,
+    HelpCircle,
+    Tag,
+    ShoppingBag,
+    Twitter,
+    Facebook,
+    Instagram,
+    Linkedin,
+} from "lucide-react";
+
+const tools = [
+    { name: "Ice Breaking", icon: Snowflake },
+    { name: "Blog Idea", icon: Lightbulb },
+    { name: "Blog Outline", icon: List },
+    { name: "Intro", icon: FileText },
+    { name: "Blog Selection", icon: BookOpen },
+    { name: "Long Content", icon: FilePlus },
+    { name: "Email Writing", icon: Mail },
+    { name: "Ads", icon: Megaphone },
+    { name: "Person Bio", icon: User },
+    { name: "Question Answer", icon: HelpCircle },
+    { name: "SEO Meta Description", icon: Tag },
+    { name: "Product Description", icon: ShoppingBag },
+    { name: "Tweets", icon: Twitter },
+    { name: "Facebook Post", icon: Facebook },
+    { name: "Insta Caption", icon: Instagram },
+    { name: "Linked Post", icon: Linkedin },
+];
+
 const WhatYouCanDo = () => {
     return (
-        <div className="max-w-[88rem] min-h-screen mx-auto flex items-start flex-coljustify-center px-3 mb-20" id='what-you-can-do'>
-            <div className="w-full h-full border border-gray-400 rounded-[50px] px-3 py-5">
+        <div
+            className="max-w-[88rem] min-h-screen mx-auto flex items-start flex-coljustify-center px-3 mb-20"
+            id="what-you-can-do"
+        >
+            <div className="w-full h-full border border-gray-400 rounded-[50px] p-10">
                 <span className="flex justify-center flex-col">
-                    <h1 data-aos='fade-up' className="text-xl md:text-2xl font-bold mb-3 self-center ">What you can do</h1>
+                    <h1
+                        data-aos="fade-up"
+                        className="text-xl md:text-2xl font-bold mb-3 self-center "
+                    >
+                        What you can do
+                    </h1>
                     <span className="self-center w-16 border-b-4 border-[#116655]"></span>
                 </span>
 
-                <p data-aos='fade-up' className="text-xl md:text-3xl font-bold text-center my-5">What type of content you can generate with Jiandikie</p>
-                <p data-aos='fade-up' className="text-sm md:text-xl text-center max-w-6xl self-center mx-auto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, </p>
+                <p
+                    data-aos="fade-up"
+                    className="text-xl md:text-3xl font-bold text-center my-5"
+                >
+                    What type of content you can generate with Jiandikie
+                </p>
+                <p
+                    data-aos="fade-up"
+                    className="text-sm md:text-xl text-center max-w-6xl self-center mx-auto"
+                >
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the
+                    industry&apos;s standard dummy text ever since the 1500s,{" "}
+                </p>
 
                 <div className=" grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-2 mt-10">
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
-                    <Box />
+                    {tools?.map((tool, idx) => (
+                        <Box key={idx} tool={tool} />
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default WhatYouCanDo;
 
-
-
-const Box = () => (
-    <div data-aos='fade-up' className="w-fit min-h-14 p-1 flex items-center gap-2 border border-gray-300 rounded-lg">
+const Box = ({ tool }) => (
+    <div
+        data-aos="fade-up"
+        className="w-full min-h-14 p-1 flex items-center gap-2 border border-gray-300 rounded-lg"
+    >
         <span className="min-w-14 h-full bg-[#DCFFF8] flex justify-center items-center rounded-md">
-            <img src={Intro} alt="" />
+            <tool.icon />
         </span>
-        <p className='text-center w-full text-sm sm:text-base lg:text-lg px-4 sm:px-6 md:px-8'>
-            Some text
+        <p className="text-center w-full text-sm sm:text-base px-4 sm:px-6 md:px-8">
+            {tool.name}
         </p>
     </div>
-)
+);
